1da2448298f15a30c6ba3419874ac2212b33d08f
Jeff <jeff.luo@henkel.com>
2023-03-20 15:39:02 +0800

    add react router,navigation and logo
    (cherry picked from commit 86e640aef85c5f66c6e2285c7df5a90bbc448af8)

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,6 +1,27 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
 const MainPage = (props: any) => {
   return (
     <div>
+      <AppBar position='fixed' >
+      <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <h1 className='hover:text-sky-400'>MainPage</h1>
     </div>
   )
