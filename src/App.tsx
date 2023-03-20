700db4305fdcdbb010ffb2f5841f6a76dc8f5b76
Jeff <jeff.luo@henkel.com>
2023-03-20 14:45:48 +0800

    enable tailwind and scss
    (cherry picked from commit 68e57dd36f0f61497d80af5bc5cd442f11a8d396)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.scss'
+import './styles/tailwind.scss';
 import MainPage from './components/MainPage/MainPage'
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App bg-slate-400'>
       <MainPage></MainPage>
     </div>
   )
