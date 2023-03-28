52960869f0aa839f58a71eaffa28c766f0f4e17b
Jeff <jeff.luo@henkel.com>
2023-03-28 16:03:19 +0800

    add generate-react-cli

---
+++ src/components/ImportBook/ImportBook.tsx
@@ -0,0 +1,8 @@
+import React, {FC} from 'react'
+import './ImportBook.scss'
+
+interface ImportBookProps {}
+
+const ImportBook: FC<ImportBookProps> = () => <div>ImportBook Component</div>
+
+export default ImportBook
---
+++ src/components/LoginDialog/LoginDialog.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './LoginDialog.scss';
+
+interface LoginDialogProps {}
+
+const LoginDialog: FC<LoginDialogProps> = () => (
+  <div className="LoginDialog">
+    LoginDialog Component
+  </div>
+);
+
+export default LoginDialog;
