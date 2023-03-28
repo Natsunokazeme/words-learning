96a85bbdcec8789e30cdfb3d7e2322c729982077
Jeff <jeff.luo@henkel.com>
2023-03-28 16:03:19 +0800

    add generate-react-cli
    (cherry picked from commit 52960869f0aa839f58a71eaffa28c766f0f4e17b)

---
+++ src/components/CameraScan/CameraScan.tsx
@@ -0,0 +1,8 @@
+import React, {FC} from 'react'
+import './CameraScan.scss'
+
+interface CameraScanProps {}
+
+const CameraScan: FC<CameraScanProps> = () => <div>CameraScan Component</div>
+
+export default CameraScan
