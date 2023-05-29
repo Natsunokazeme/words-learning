37730864133df14b73527b9aa8df43d608646524
Jeff <jeff.luo@henkel.com>
2023-05-29 14:22:38 +0800

    feat(): set active text(5.29)
    (cherry picked from commit 6e1c377c0a6b76a371533f6884aeb738b0047ba2)

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,35 +1,54 @@
 import './Clock.scss'
 import React, {useState} from 'react'
+import {useEffect} from 'react'
 
 const Clock = () => {
   const [curTime, setCurTime] = useState(new Date())
-  setTimeout(() => {
-    setCurTime(new Date())
-  }, 1000)
+  useEffect(() => {
+    setInterval(() => {
+      setCurTime(new Date())
+    }, 1000)
+  }, [])
 
-  const circleElements = (size: number, radius: number) =>
+  const circleElements = (
+    size: number,
+    radius: number,
+    accurate: number,
+    label?: string
+  ) =>
     Array.from({length: size}, (_, index) => (
       <span
-        className='inline-block bg-white w-8 h-8 border-2 absolute top-0 bottom-0 left-0 right-0 m-auto'
+        className='inline-block bg-transparent w-16 h-4 absolute top-0 bottom-0 left-0 right-0 m-auto transition-transform border-0'
         style={{
+          color: index === accurate ? 'white' : 'grey',
           transform: `translate(${
-            Math.sin(((index + 1) / size) * 2 * Math.PI) * radius
-          }px,${Math.cos(((index + 1) / size) * 2 * Math.PI) * -radius}px)`,
+            Math.sin(((index - accurate) / size + 1 / 4) * 2 * Math.PI) * radius
+          }px,${
+            Math.cos(((index - accurate) / size + 1 / 4) * 2 * Math.PI) *
+            -radius
+          }px) rotate(${(index - accurate) / size}turn)`,
         }}
         key={index}
       >
-        {index + 1}
+        {index.toString().padStart(2, '0')}
+        {label}
       </span>
     ))
 
   return (
-    <div className='clock'>
+    <div className='clock-wrapper'>
       <h1>{curTime.toISOString()}</h1>
       {curTime.getSeconds()}
       <div className='clock'>
-        <div className='seconds'>{circleElements(60, 400)}</div>
-        <div className='minutes'>{circleElements(60, 300)}</div>
-        <div className='hours'>{circleElements(12, 150)}</div>
+        <div className='seconds'>
+          {circleElements(60, 200, curTime.getSeconds(), '秒')}
+        </div>
+        <div className='minutes'>
+          {circleElements(60, 150, curTime.getMinutes(), '分')}
+        </div>
+        <div className='hours'>
+          {circleElements(24, 100, curTime.getHours(), '时')}
+        </div>
       </div>
     </div>
   )
