938dbb82e244068cc99d32abad4d05fac4624783
Jeff <jeff.luo@henkel.com>
2023-07-07 18:30:34 +0800

    feat(): extract camera (7.7)

--- src/components/CameraScan/CameraScan.tsx
+++ src/components/CameraScan/CameraScan.tsx
@@ -2,30 +2,28 @@ import React, {FC, useRef, useState} from 'react'
 import './CameraScan.scss'
 import {useEffect} from 'react'
 import {CircularProgress} from '@mui/material'
-import PhotoCameraIcon from '@mui/icons-material/PhotoCamera'
-import DownloadIcon from '@mui/icons-material/Download'
-// import ChangeCircleIcon from '@mui/icons-material/ChangeCircle'
-import UploadIcon from '@mui/icons-material/Upload'
+import CameraRoundedIcon from '@mui/icons-material/CameraRounded'
 
-import 'cropperjs/dist/cropper.css'
-import CropperModal from '../CropperModal/CropperModal'
-
-interface CameraScanProps {}
+interface CameraScanProps {
+  show: boolean
+  drawCanvas: (video: HTMLVideoElement, hidden: boolean) => void
+  setShow: (show: boolean) => void
+}
 
 interface CustomMediaDeviceInfo
   extends Pick<MediaDeviceInfo, Exclude<keyof MediaDeviceInfo, 'toJSON'>> {
   active: boolean
 }
 
-const CameraScan: FC<CameraScanProps> = () => {
+const CameraScan: FC<CameraScanProps> = (prop: CameraScanProps) => {
   const [loading, setLoading] = useState(true)
   const videoRef = useRef<HTMLVideoElement>(null)
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-  const uploadRef = useRef<HTMLInputElement>(null)
-  const [cropperImg, setCropperImg] = useState('')
+  // const canvasRef = useRef<HTMLCanvasElement>(null)
+  // const uploadRef = useRef<HTMLInputElement>(null)
+  // const [cropperImg, setCropperImg] = useState('')
   const [stream, setStream] = useState<MediaStream>()
   const [cameras, setCameras] = useState<CustomMediaDeviceInfo[]>([])
-  const [open, setOpen] = useState(false)
+  // const [open, setOpen] = useState(false)
 
   useEffect(() => {
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
@@ -59,6 +57,7 @@ const CameraScan: FC<CameraScanProps> = () => {
   const startCamera = (constraints: MediaStreamConstraints) => {
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
       setStream(stream)
+      // todo when show is false,  video is false
       const video = videoRef.current as HTMLVideoElement
       video.srcObject = stream
       video.onloadeddata = () => {
@@ -72,18 +71,8 @@ const CameraScan: FC<CameraScanProps> = () => {
 
   const takePhoto = () => {
     const video = videoRef.current as HTMLVideoElement
-    drawCanvas(video, true)
-    video.hidden = true
-    setOpen(true)
-  }
-
-  const savePhoto = () => {
-    const canvas = canvasRef.current as HTMLCanvasElement
-    const url = canvas.toDataURL()
-    const link = document.createElement('a')
-    link.href = url
-    link.download = new Date().toISOString() + '.png'
-    link.click()
+    prop.drawCanvas(video, true)
+    prop.setShow(false)
   }
 
   const changeCamera = async (id: string) => {
@@ -101,149 +90,53 @@ const CameraScan: FC<CameraScanProps> = () => {
     stream?.getTracks().forEach((track) => track.stop())
   }
 
-  const uploadImage = () => {
-    const upload = uploadRef.current as HTMLInputElement
-    upload.click()
-  }
-
-  const convertFileToImage = (file: File) => {
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => {
-      const img = new Image()
-      img.src = reader.result as string
-      img.onload = () => {
-        setCropperImg(img.src)
-        setOpen(true)
-      }
-    }
-  }
-
-  const drawCanvas = (
-    element: HTMLImageElement | HTMLVideoElement,
-    mirror?: boolean
-  ) => {
-    const canvas = canvasRef.current as HTMLCanvasElement
-    const ctx = canvas.getContext('2d')
-    if (ctx) {
-      canvas.height = element.height
-      canvas.width = element.width
-      ctx.drawImage(element, 0, 0)
-      if (mirror) {
-        ctx.save()
-        ctx.scale(-1, 1)
-        ctx.drawImage(element, -element.width, 0)
-        // restore to original state
-        ctx.restore()
-      }
-    }
-    setCropperImg(canvas.toDataURL())
-    return canvas
-  }
-
-  const updateCanvas = (element: HTMLImageElement | HTMLVideoElement) => {
-    const canvas = canvasRef.current as HTMLCanvasElement
-    const ctx = canvas.getContext('2d')
-    if (ctx) {
-      ctx.drawImage(element, 0, 0)
-    }
-  }
-
-  const cropImage = (imgUrl: string) => {
-    const canvas = canvasRef.current as HTMLCanvasElement
-    const ctx = canvas.getContext('2d')
-    if (ctx) {
-      const img = new Image()
-      img.src = imgUrl
-      img.onload = () => {
-        drawCanvas(img)
-        setOpen(false)
-      }
-    }
-  }
-
-  //todo cropper for canvas
-  //todo use second canvas for cropper and compress them into one
   //todo count down for camera
   // todo empty image
   //todo hide camera after canvas is loaded
 
   return (
     <>
-      {loading ? (
-        <CircularProgress size={100} className='fixed left-1/2 top-1/2' />
-      ) : null}
-      <div
-        className={`camera-wrapper flex items-center gap-5 ${
-          loading ? 'hidden' : ''
-        }`}
-      >
-        <video
-          id='video'
-          ref={videoRef}
-          className={`max-w-full camera`}
-        ></video>
-        <CropperModal
-          show={open}
-          setShow={setOpen}
-          cropperImg={cropperImg}
-          afterCrop={cropImage}
-        ></CropperModal>
+      {/* {prop.show ? ( */}
+      <div className={`${prop.show ? '' : 'invisible'} camera-modal`}>
+        {loading ? (
+          <CircularProgress size={100} className='fixed left-1/2 top-1/2' />
+        ) : null}
+        <div
+          className={`camera-wrapper flex items-center gap-5 ${
+            loading ? 'hidden' : ''
+          }`}
+        >
+          <video
+            id='video'
+            ref={videoRef}
+            className={`max-w-full camera`}
+          ></video>
+        </div>
+        <div className='actions flex justify-center mt-10'>
+          <button
+            onClick={() => {
+              takePhoto()
+            }}
+          >
+            <CameraRoundedIcon
+              fontSize='large'
+              color='primary'
+            ></CameraRoundedIcon>
+          </button>
+        </div>
+        {cameras.map((camera) => (
+          <li
+            onClick={() => {
+              changeCamera(camera.deviceId)
+            }}
+            className={`${camera.active ? 'bg-pink-100' : ''}`}
+            key={camera.deviceId}
+          >
+            {camera.label.split('(')[0]}
+          </li>
+        ))}
       </div>
-      <div className='actions flex justify-center mt-10'>
-        <button
-          onClick={() => {
-            takePhoto()
-          }}
-        >
-          <PhotoCameraIcon fontSize='large' color='primary'></PhotoCameraIcon>
-        </button>
-        <button
-          onClick={() => {
-            uploadImage()
-          }}
-        >
-          <UploadIcon fontSize='large' color='primary'></UploadIcon>
-        </button>
-        <button
-          onClick={() => {
-            savePhoto()
-          }}
-        >
-          <DownloadIcon fontSize='large' color='primary'></DownloadIcon>
-        </button>
-      </div>
-      {cameras.map((camera) => (
-        <li
-          onClick={() => {
-            changeCamera(camera.deviceId)
-          }}
-          className={`${camera.active ? 'bg-pink-100' : ''}`}
-          key={camera.deviceId}
-        >
-          {camera.label.split('(')[0]}
-        </li>
-      ))}
-      <div className='canvas-container'>
-        <p>Preview</p>
-        <canvas ref={canvasRef} className='max-w-full'></canvas>
-      </div>
-      <input
-        type='file'
-        accept='image/*'
-        className='hidden'
-        ref={uploadRef}
-        onChange={(e) => {
-          let files = e.target.files
-          if (files && files.length > 0) {
-            // only one file now
-            convertFileToImage(files[0])
-            ;(uploadRef.current as HTMLInputElement).value = ''
-          } else {
-            // todo empty files
-          }
-        }}
-      />
+      {/* ) : null} */}
     </>
   )
 }
--- src/pages/ImagePage/ImagePage.tsx
+++ src/pages/ImagePage/ImagePage.tsx
@@ -1,10 +1,155 @@
+import {useRef, useState} from 'react'
 import CameraScan from '../../components/CameraScan/CameraScan'
 import './ImagePage.scss'
+import CropperModal from '../../components/CropperModal/CropperModal'
+
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera'
+import DownloadIcon from '@mui/icons-material/Download'
+import UploadIcon from '@mui/icons-material/Upload'
 
 const ImagePage = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const uploadRef = useRef<HTMLInputElement>(null)
+  const [cropperImg, setCropperImg] = useState('')
+  const [openCropper, setOpenCropper] = useState(false)
+  const [openCamera, setOpenCamera] = useState(false)
+
+  const uploadImage = () => {
+    const upload = uploadRef.current as HTMLInputElement
+    upload.click()
+  }
+
+  const saveCanvas = () => {
+    const canvas = canvasRef.current as HTMLCanvasElement
+    const url = canvas.toDataURL()
+    const link = document.createElement('a')
+    link.href = url
+    link.download = new Date().toISOString() + '.png'
+    link.click()
+  }
+
+  const convertFileToImage = (file: File) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => {
+      const img = new Image()
+      img.src = reader.result as string
+      img.onload = () => {
+        setCropperImg(img.src)
+        setOpenCropper(true)
+      }
+    }
+  }
+
+  const drawCanvas = (
+    element: HTMLImageElement | HTMLVideoElement,
+    mirror?: boolean
+  ) => {
+    const canvas = canvasRef.current as HTMLCanvasElement
+    const ctx = canvas.getContext('2d')
+    if (ctx) {
+      canvas.height = element.height
+      canvas.width = element.width
+      ctx.drawImage(element, 0, 0)
+      if (mirror) {
+        ctx.save()
+        ctx.scale(-1, 1)
+        ctx.drawImage(element, -element.width, 0)
+        // restore to original state
+        ctx.restore()
+      }
+    }
+    setCropperImg(canvas.toDataURL())
+    return canvas
+  }
+
+  const updateCanvas = (element: HTMLImageElement | HTMLVideoElement) => {
+    const canvas = canvasRef.current as HTMLCanvasElement
+    const elementWidth = element.width
+    // const elementHeight = element.height
+    const canvasWidth = canvas.width
+    const canvasHeight = canvas.height
+    const ctx = canvas.getContext('2d')
+    //todo assign start position
+    if (ctx) {
+      ctx.drawImage(element, canvasWidth - elementWidth, canvasHeight / 10)
+    }
+  }
+
+  const cropImage = (imgUrl: string) => {
+    const canvas = canvasRef.current as HTMLCanvasElement
+    const ctx = canvas.getContext('2d')
+    if (ctx) {
+      const img = new Image()
+      img.src = imgUrl
+      img.onload = () => {
+        if (canvas.height !== 0) {
+          updateCanvas(img)
+        } else {
+          drawCanvas(img)
+        }
+        setOpenCropper(false)
+      }
+    }
+  }
   return (
     <div className='image-page'>
-      <CameraScan></CameraScan>
+      <div className='canvas-container'>
+        <p>Preview</p>
+        <canvas ref={canvasRef} className='max-w-full'></canvas>
+      </div>
+      <div className='actions flex justify-center mt-10'>
+        <button
+          onClick={() => {
+            //todo open camera
+            setOpenCamera(true)
+          }}
+        >
+          <PhotoCameraIcon fontSize='large' color='primary'></PhotoCameraIcon>
+        </button>
+        <button
+          onClick={() => {
+            uploadImage()
+          }}
+        >
+          <UploadIcon fontSize='large' color='primary'></UploadIcon>
+        </button>
+        <button
+          onClick={() => {
+            //todo download image
+            saveCanvas()
+          }}
+        >
+          <DownloadIcon fontSize='large' color='primary'></DownloadIcon>
+        </button>
+      </div>
+      <CameraScan
+        show={openCamera}
+        drawCanvas={drawCanvas}
+        setShow={setOpenCamera}
+      ></CameraScan>
+      <CropperModal
+        show={openCropper}
+        setShow={setOpenCropper}
+        cropperImg={cropperImg}
+        afterCrop={cropImage}
+      ></CropperModal>
+      <input
+        type='file'
+        accept='image/*'
+        className='hidden'
+        ref={uploadRef}
+        onChange={(e) => {
+          let files = e.target.files
+          if (files && files.length > 0) {
+            // only one file now
+            convertFileToImage(files[0])
+            ;(uploadRef.current as HTMLInputElement).value = ''
+          } else {
+            // todo empty files
+          }
+        }}
+      />
     </div>
   )
 }
